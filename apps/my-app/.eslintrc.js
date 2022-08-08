00aa30333a89ba53932d6d02359e6ce091ea35d7
juan-ea-garcia-tribal <109571470+SiTribal@users.noreply.github.com>
2022-08-08 23:23:21 +1000

    react linting config

---
+++ apps/my-app/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    // This tells ESLint to load the config from the package `eslint-config-acme`
+    extends: ["acme"],
+    settings: {
+      next: {
+        rootDir: ["apps/*/"],
+      },
+    },
+  };
+  
